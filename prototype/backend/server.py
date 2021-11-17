c3fea6ffbc542597f942dc1254d5b2d826c9744f
HectorFRamirez <hectorr@bu.edu>
2021-11-17 00:29:30 -0500

    added recipe endpoint

--- prototype/backend/server.py
+++ prototype/backend/server.py
@@ -3,10 +3,11 @@ import requests
 
 app = Flask(__name__)
 
-
 '''
  GET ALL PLANTS or SEARCH FOR ALL PLANTS THAT MATCH A TEXT
 '''
+
+
 @app.route("/plants", methods=['POST', 'GET'])
 def plants():
     if request.method == 'POST':
@@ -19,6 +20,23 @@ def plants():
         API_URL = "https://tropicalfruitandveg.com/api/tfvjsonapi.php?search=all"
         response = requests.get(API_URL)
         return response.json()
+
+
+@app.route("/recipes", methods=['GET'])
+def recipes():
+    body = request.get_json()
+    recipe = body["recipe"]
+
+    API_URL = "https://edamam-recipe-search.p.rapidapi.com/search?q=" + recipe
+    headers = {
+        'x-rapidapi-host': "edamam-recipe-search.p.rapidapi.com",
+        'x-rapidapi-key': "1fb806462bmsh644af73596e24bfp18ef67jsn9c7b02e90738"
+    }
+
+    response = requests.get(API_URL, params=headers)
+    return response.json()
+
+    # response = requests.request("GET", url, headers=headers, params=querystring)
 
 
 if __name__ == "__main__":
