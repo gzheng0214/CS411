18fa02e7cf373a24a8cf7911dbf0fbb885283950
HectorFRamirez <hectorr@bu.edu>
2021-11-22 16:22:50 -0500

    implemented oath2 with google

--- prototype/backend/server.py
+++ prototype/backend/server.py
@@ -1,8 +1,26 @@
 from flask import Flask, request
 import requests
+import os
+import pathlib
+from flask import Flask, session, abort, redirect, request
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 
 app = Flask(__name__)
+app.secret_key = "Remember2EatFruits:)"
+GOOGLE_CLIENT_ID = "257365493228-jd74c6qimgo4pocp9h7cko2s222pcg9r.apps.googleusercontent.com"
 
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
 '''
  GET ALL PLANTS or SEARCH FOR ALL PLANTS THAT MATCH A TEXT
 '''
@@ -38,6 +56,36 @@ def recipes():
 
     # response = requests.request("GET", url, headers=headers, params=querystring)
 
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/loggedin")
+
+@app.route("/loggedin")
+def loggedin():
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(debug=True)
